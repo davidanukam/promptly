37f276269efc32eeb3375d2d36d1929d66fc5bbe
Duzzenn <109883788+davidanukam@users.noreply.github.com>
2025-12-09 03:32:47 -0500

    Setup NextJS and Shadcn

---
+++ data/meta.ts
@@ -0,0 +1,3 @@
+export const GITHUB_URL = "https://github.com/davidanukam/promptly";
+export const DISCORD_URL = "https://discord.com/invite/pQZ7turwT7";
+export const SPONSOR_URL = "https://www.linkedin.com/in/david-anukam";
